f8c8fa983f4dab6c5a0fdfdfd9822740146d4310
Tomás Lucas Vilaboa <tvilaboa@hotmail.com>
2014-03-26 20:49:29 -0300

    Adds jdbc driver and class to obtain connection to DB.

---
+++ src/main/java/database/DatabaseConnection.java
@@ -0,0 +1,34 @@
+package database;
+
+
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: Tomás Vilaboa
+ * Project: Trav
+ * Date: 26/03/14
+ * Time: 20:35
+ */
+public class DatabaseConnection {
+
+    private Connection connection;
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void establishConnection(String server, String user, String password) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(server, user, password);
+        } catch (Exception e) {
+            System.out.println("Connection failed");
+            JOptionPane.showInternalMessageDialog(null, "Connection failed");
+            e.printStackTrace();
+        }
+    }
+}
--- web/menu.jsp
+++ web/menu.jsp
@@ -3,7 +3,7 @@
     <a href="<%=response.encodeURL("index.jsp")%>">Home</a>
     | <a href="<%=response.encodeURL("securePage.jsp")%>">Secure Page</a>
     | <a href="<%=response.encodeURL("forbiddenPage.jsp")%>">Forbidden Secure Page</a>
-    | <a href="<%=response.encodeURL("loginForm.jsp")%>">Direct Login</a>
+    | <a href="<%=response.encodeURL("loginForm.jsp")%>">Direct servlets.Login</a>
     | <a href="<%=response.encodeURL("logout.jsp")%>">Logout</a>
     ]
 
